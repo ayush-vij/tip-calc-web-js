6ca448663ba815309d73af0f8b10081924d383d1
ayush-vij <ayushvij@live.in>
2021-04-29 00:44:33 -0700

    adding tax calculation

--- js.js
+++ js.js
@@ -2,10 +2,31 @@ document.getElementById("btn15").addEventListener("click",calc);
 document.getElementById("btn175").addEventListener("click",calc);
 document.getElementById("btn20").addEventListener("click",calc);
 document.getElementById("btncalc").addEventListener("click",calc);
+document.getElementById("CA").addEventListener("click",calc);
+document.getElementById("CO").addEventListener("click",calc);
+document.getElementById("DC").addEventListener("click",calc);
+document.getElementById("WA").addEventListener("click",calc);
 
 function calc(event){
     let tipPercent = 0;
     let buttonPressed = event.target.id;
+    let taxPercent = 0;
+    let stateSelect = event.target.id;
+
+    switch(stateSelect){
+        case "CA":
+            taxPercent = 0.1025;
+            break;
+        case "CO":
+            taxPercent = 0.735;
+            break;
+        case "DC":
+            taxPercent = 0.10;
+            break;
+        case "WA":
+            taxPercent = 0.101;
+            break;
+    }
 
     switch (buttonPressed){
         case "btn15":
@@ -23,11 +44,13 @@ function calc(event){
 
     let billAmount = document.getElementById("billAmount").value;
     billAmount = parseFloat(billAmount);
+    let tax = billAmount * taxPercent;
     let tip = billAmount * tipPercent;
-    let total = tip + billAmount;
-    reportTotals(tip,total);
+    let total = tax + tip + billAmount;
+    reportTotals(tax,tip,total);
 
-    function reportTotals(tip, total){
+    function reportTotals(tax, tip, total){
+        document.getElementById("taxAmount").innerHTML = "<strong>$" + tax + "</strong>";
         document.getElementById("tipAmount").innerHTML = "<strong>$" + tip + "</strong>";
         document.getElementById("total").innerHTML = "<strong>$" + total + "</strong>";
     }
